40b2a3c0e75cd99fa1a7a0ff1b7831fa6b213873
RisNag777 <risnag1995@gmail.com>
2026-01-06 15:56:38 -0500

    [ADD] logic to trim the list of candidates based on feedback

--- src/game_logic.py
+++ src/game_logic.py
@@ -30,3 +30,47 @@ def get_feedback(guess, solution):
             solution_counts[guess[i]] -= 1
 
     return feedback
+
+
+def cow_bull_absent(guess, feedback):
+    cows = {}
+    bulls = {}
+    absent = set()
+    for f in range(len(feedback)):
+        char = guess[f]
+        if feedback[f] == 2:
+            bulls[char] = f
+        elif feedback[f] == 1:
+            cows[char] = f
+        else:
+            absent.add(char)
+    return cows, bulls, absent
+
+
+def trim_list(guess, feedback, candidates):
+    cows, bulls, absent = cow_bull_absent(guess, feedback)
+    if len(absent) == 0:
+        abs_c = []
+        for candidate in candidates:
+            if all(letter not in candidate for letter in absent):
+                abs_c.append(candidate)
+    else:
+        abs_c = candidates
+    if bulls.keys() and not abs_c:
+        bull_c = []
+        for candidate in abs_c:
+            if all(candidate[pos] == letter for letter, pos in bulls.items()):
+                bull_c.append(candidate)
+    else:
+        bull_c = abs_c
+    if cows.keys() and not bull_c:
+        cow_c = []
+        for candidate in bull_c:
+            if all(
+                letter in candidate and candidate[pos] != letter
+                for letter, pos in cows.items()
+            ):
+                cow_c.append(candidate)
+    else:
+        cow_c = bull_c
+    return cow_c
