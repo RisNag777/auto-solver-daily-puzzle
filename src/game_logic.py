b12ebe2aaccce33938f0c23c85327263925bec2a
RisNag777 <risnag1995@gmail.com>
2026-01-06 15:42:10 -0500

    [FIX] black reformatting

--- src/game_logic.py
+++ src/game_logic.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 def retrieve_word_list():
     data_folder = os.getenv("DATA_FOLDER", "data")
     words_path = f"{data_folder}/words.txt"
-    
+
     with open(words_path, "r") as file:
         candidates = [line.rstrip("\n") for line in file]
         return candidates
-
